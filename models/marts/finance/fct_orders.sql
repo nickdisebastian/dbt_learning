2d06f824548c89f0ed5fdf1ac6466c71312828ed
Nick DiSebastian <nickdisebastian@gmail.com>
2024-10-24 17:31:06 +0000

    reformat amount field

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,6 +1,30 @@
-select 
-    c.order_id,
-    c.customer_id,
-    p.amount
-from {{ ref('stg_jaffle_shop__orders') }} c
-LEFT JOIN {{ ref('stg_stripe__payments') }} p on c.order_id = p.order_id
+with orders as (
+    select *
+    from {{ ref('stg_jaffle_shop__orders') }}
+),
+
+payments as (
+    select *
+    from {{ ref('stg_stripe__payments') }}
+),
+
+order_payments as (
+    select 
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by order_id
+),
+
+final as(
+    select  
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * 
+from final
--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,54 +1,35 @@
-with customers as (
+with
+    customers as (select * from {{ ref("stg_jaffle_shop__customers") }}),
 
-    select * from {{ ref('stg_jaffle_shop__customers') }}
+    orders as (select * from {{ ref("fct_orders") }}),
 
-),
+    customer_orders as (
+        select
+            customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
+        from orders
+        group by 1
+    ),
 
-orders as (
+    final as (
 
-    select 
-        o.order_id,
-        o.customer_id,
-        o.order_date,
-        o.status,
-        sum(p.amount) as order_total
-     from {{ ref('stg_jaffle_shop__orders') }} o
-     left join {{ ref('stg_stripe__payments') }} p on o.order_id = p.order_id
-     group by 1,2,3,4
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
-),
+        from customers
 
+        left join customer_orders using (customer_id)
 
-customer_orders as (
+    )
 
-    select
-        customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders,
-        sum(order_total) as lifetime_value
-
-    from orders
-
-    group by 1
-
-),
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(customer_orders.lifetime_value,0) as lifetime_value
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
-)
-
-select * from final
+select *
+from final
