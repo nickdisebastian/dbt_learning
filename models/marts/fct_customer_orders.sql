8af3f475e981bfe7a8174c095881cbfdcdcc0cd4
Nick DiSebastian <nickdisebastian@gmail.com>
2024-10-29 14:13:10 +0000

    simplify remaining subqueries with window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -38,17 +38,6 @@ paid_orders as (
     left join customers on orders.user_id = customers.id
 ),
 
-customer_orders as (
-    select
-        customers.id as customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(orders.id) as number_of_orders
-    from customers
-    left join
-        orders on orders.user_id = customers.id
-    group by 1
-),
 
 final as (
     select
@@ -62,15 +51,20 @@ final as (
         paid_orders.customer_last_name,
         row_number() over (order by paid_orders.order_id) as transaction_seq,
         row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
-        case
-            when customer_orders.first_order_date = paid_orders.order_placed_at 
-            then 'new' else 'return'
-        end as nvsr,
-        round(sum(paid_orders.total_amount_paid) 
-        over (partition by paid_orders.customer_id), 2) as customer_lifetime_value,
-        customer_orders.first_order_date as fdos
-from paid_orders
-left join customer_orders using (customer_id)
-order by order_id)
+        case 
+            when (
+                rank() over (
+                    partition by paid_orders.customer_id 
+                    order by paid_orders.order_placed_at, paid_orders.order_id) 
+                = 1)
+                then 'new' 
+                else 'return' end as nvsr,
+        sum(paid_orders.total_amount_paid) over (
+            partition by paid_orders.customer_id) as customer_lifetime_value,
+        first_value(paid_orders.order_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+            ) as fdos
+from paid_orders)
 
 select * from final 
